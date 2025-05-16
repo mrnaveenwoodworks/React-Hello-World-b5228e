a944d3e175a8a143ff9bc3ac620205f94f7914ee
mrnaveenwoodworks <mrnaveenwoodworks@gmail.com>
2025-05-16 14:25:21 +0530

    CODEMATE.ai - upload codebase

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <h1 className="text-9xl font-bold">HELLO</h1>
+    </div>
+  );
+}
+
+export default App;
